b88fa13b5f7c8a2ca981a78e66acd3f717851ac2
Shivam Bansal <bansal.shivam1216@gmail.com>
2026-09-08 17:28:50 +0530

    Day 2 Pattern Completed

--- 3rd Year/New_Sep_26/Day1LCQ.java
+++ 3rd Year/New_Sep_26/Day1LCQ.java
@@ -1,14 +1,11 @@
 
-public class Day1 {
+public class Day1LCQ {
 
     public static void main(String[] args) {
 
         // // // // // // Add 2 Numbers (LeetCode Q2) // // // // // //
         // // // // // // Middle Element of Linked List (LeetCode Q876) // // // // // //
         // // // // // // Palindrome Linked List (LeetCode Q234) // // // // // //
-
         // // // // // // Merge 2 Sorted Lists (LeetCode Q21) // // // // // //
-
-
     }
 }
---
+++ 3rd Year/New_Sep_26/Day2Pattern.java
@@ -0,0 +1,98 @@
+
+import java.util.*;
+
+public class Day2Pattern {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // // // // (Brute Force) Pattern 1 // // //
+        // System.out.println("*");
+        // System.out.println("* *");
+        // System.out.println("* * * ");
+        // System.out.println("* * * *");
+        // System.out.println("* * * * *");
+        // // // // (Brute Force) Pattern 2 // // //
+        // System.out.println("*****   ****");
+        // System.out.println("*       *   *");
+        // System.out.println("*       *   *");
+        // System.out.println("*****   ****");
+        // System.out.println("    *   *   *");
+        // System.out.println("    *   *   *");
+        // System.out.println("*****   ****");
+        // // // // Pattern 0 // // //
+        // for (int i = 0; i < 5; i++) {
+        //     for (int j = 0; j < 5; j++) {
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+        // =============================================================================== //
+        System.out.print("Enter the number of rows: ");
+        int n = sc.nextInt();
+
+        System.out.println();
+
+        // int[] arr = {4, 9, 2, 7, 5};
+        // // // // Pattern 1  // // //
+        // for (int row = 0; row < n; row++) {
+        //     for (int col = 0; col <= row; col++) {
+        //         System.out.print((row + 1) * (col + 1) + " ");
+        //         // System.out.print("* ");
+        //         // System.out.print((col + 1) + " ");
+        //         // char c = (char) (col + 65);
+        //         // System.out.print((c) + " ");
+        //         // System.out.print("(" + row + ", " + col + ") ");
+        //         // System.out.print(arr[col] + " ");
+        //     }
+        //     System.out.println();
+        // }
+        // for (int i = n - 1; i >= 0; i--) {
+        //     for (int j = n - 1; j >= i; j--) {
+        //         System.out.print("(" + (i + 1) + ", " + (j + 1) + ") ");
+        //     }
+        //     System.out.println();
+        // }
+        // System.out.println("-----------------------------");
+        // // // // Pattern 2  // // //
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = n - 1; j >= i; j--) {
+        //         // System.out.print("* ");
+        //         // System.out.print(j + 1 + " ");
+        //         System.out.print(i + 1 + " ");
+        //     }
+        //     System.out.println();
+        // }
+        // for (int i = n; i > 0; i--) {
+        //     for (int j = i; j > 0; j--) {
+        //         System.out.print((i * j) + " ");
+        //     }
+        //     System.out.println();
+        // }
+        // int[] arr = {8, 6, 4, 2, 9};
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < n; j++) {
+        //         if (i == 0 || i == n - 1 || j == 0 || j == n - 1) {
+        //             // System.out.print("*");
+        //             System.out.print(arr[i] + " ");
+        //             // System.out.print(arr[j] + " ");
+        //         } else {
+        //             System.out.print("  ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (i == j || i + j == n - 1) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
